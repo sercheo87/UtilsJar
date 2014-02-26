b4493727921030c7cbde0617edca03af797e951b
Sergio <chancaysergio@gmail.com>
2014-02-25 21:27:40 -0800

    Estructura del Proyecto

---
+++ SchUtils/test/Utils/Files/FindUtilTest.java
@@ -0,0 +1,14 @@
+package Utils.Files;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FindUtilTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
